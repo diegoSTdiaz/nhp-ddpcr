4d940dfa0a15461b46bc5d6b783e5f86100d0329
diegoSTdiaz <diego.diaz@sonothera.com>
2025-11-29 20:07:15 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,5 @@
+Python
+
 import streamlit as st
 import pandas as pd
 import plotly.graph_objects as go
@@ -172,3 +174,4 @@ if plate_file and sample_file:
 
 else:
     st.info("Upload Plate Layout + Sample Info to begin. Add results CSV when run is done.")
+
